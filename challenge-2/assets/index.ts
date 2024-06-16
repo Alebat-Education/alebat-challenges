be5aa07a3f22e014ef24adda9c3e093b6dc149d4
Rodrigo Herranz <rodrigoasime@gmail.com>
2024-06-16 19:20:14 +0200

    chore(assets): add svg

---
+++ challenge-2/assets/index.ts
@@ -0,0 +1,6 @@
+import CheckCircleSvg from './check_circle.svg'
+import ErrorSvg from './error.svg'
+import InfoSvg from './info.svg'
+import WarningSvg from './warning.svg'
+
+export { CheckCircleSvg, ErrorSvg, InfoSvg, WarningSvg }
